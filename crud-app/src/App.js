7a481abfda022c730fa652c88e8579e9c1666741
Abdul Razzaq <abdulrazzaqchohan1@gmail.com>
2023-09-13 16:42:07 +0500

    color picker

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route path="/edit" element={<UpdateOperation />} />
         </Routes>
       </BrowserRouter> */}
-      <ColorPicker />
+      <DataTable />
     </div>
   );
 }
--- crud-app/src/components/ColorPicker/ColorPiker.jsx
+++ crud-app/src/components/ColorPicker/ColorPiker.jsx
@@ -1,23 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./picker.css";
 import { GithubPicker, AlphaPicker } from "react-color";
 
 function ColorPicker() {
   // State to store the selected color
   const [selectedColor, setSelectedColor] = useState("#f00");
   const [showColorPicker, setShowColorPicker] = useState(false);
+  const [selectColor, setSelectColor] = useState({
+    r: 255, // Red component (0-255)
+    g: 0, // Green component (0-255)
+    b: 0, // Blue component (0-255)
+    a: 1, // Alpha component (0-1, fully opaque by default)
+  });
 
+  useEffect(() => {
+    // Initialize selectedColor and selectColor to red when the component mounts
+    setSelectedColor({
+      r: 255,
+      g: 0,
+      b: 0,
+      a: 1,
+    });
+  }, []);
   // Function to handle color change
   const handleColorChange = (color) => {
-    setSelectedColor(color.hex);
+    setSelectedColor(color.rgb);
+    setSelectColor(color.rgb);
+    // console.log(color);
+  };
+  const handleColorChange2 = (color) => {
+    setSelectColor(color.rgb);
+    setSelectedColor(color.rgb);
+    // console.log(color.rgb);
   };
 
+  // Define your custom colors
+  const customColors = [
+    "#FF5733",
+    "#33FF57",
+    "#3366FF",
+    "#FFFF33",
+    "#E5FFCC",
+    "#E0E0E0",
+    "#FF00FF",
+    "#800000",
+    "#FFA500",
+    "#556B2F",
+    "#00FF00",
+    "#006400",
+    "#90EE90",
+    "#008080",
+    "#00FFFF",
+    "#40E0D0",
+    "#B0E0E6",
+    "#00BFFF",
+  ];
   return (
     <div>
-      <h1 style={{ color: selectedColor }}>Color Picker</h1>
+      <h1
+        style={{
+          color: `rgba(${selectedColor.r}, ${selectedColor.g}, ${selectedColor.b}, ${selectedColor.a})`,
+        }}
+      >
+        Color Picker
+      </h1>
       <div>
         <div
           style={{
-            backgroundColor: selectedColor,
+            color: `rgba(${selectedColor.r}, ${selectedColor.g}, ${selectedColor.b}, ${selectedColor.a})`,
+            backgroundColor: `rgba(${selectedColor.r}, ${selectedColor.g}, ${selectedColor.b}, ${selectedColor.a})`,
             width: "100px",
             height: "40px",
             cursor: "pointer",
@@ -38,14 +89,15 @@ function ColorPicker() {
               <GithubPicker
                 color={selectedColor}
                 onChange={handleColorChange}
+                colors={customColors} // Add your custom colors here
               />
             )}
           </div>
           <div style={{ marginTop: "4px" }}>
             {showColorPicker && (
               <AlphaPicker
-                color={selectedColor}
-                onChange={handleColorChange}
+                color={selectColor}
+                onChange={handleColorChange2}
                 width="195px"
               />
             )}
